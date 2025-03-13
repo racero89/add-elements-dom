823e9dd3055a69399fff6efad81b9dc9c61c4884
racero89 <monica.racero@gmail.com>
2025-03-13 18:24:21 +0100

    ChallengeDom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
- // Aquí tu código
+// Aquí tu código
+const lista = document.getElementById("lista");
+const boton = document.getElementById("agregar");
+
+boton.onclick = function () {
+  let nuevoElemento = document.createElement("li");
+  nuevoElemento.textContent = "Nuevo elemento"; // Apuntes: textContent permite poner texto <li>
+
+  lista.appendChild(nuevoElemento); // Apuntes: Agrega el nuevo <li> a la lista.
+};
